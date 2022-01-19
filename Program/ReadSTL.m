5769761919b2c13124d38f06a76afae90976ec1b
Nicolas Broguiere <36864883+nbroguiere@users.noreply.github.com>
2022-01-19 18:36:53 +0100

    ReadSTL tolerates both unix and windows endlines

--- Program/ReadSTL.m
+++ Program/ReadSTL.m
@@ -1,19 +1,17 @@
-function [ triangles, normals, name] = ReadSTL(name)
-%READSTL opens an ASCII stereolithography file into Matlab. 
-% * name is the file name as a string. 
-% * triangles is a matrix. Each line describes a triangular facet in the
-% form: x1 y1 z1 x2 y2 z2 x3 y3 z3
-% * normals are in the form xn yn zn
-% 
-% Copyright Nicolas Broguiere. First version 2012. Last edit 2019. 
+function [ triangles, normals, name ] = ReadSTL(name)
+% Read an ASCII *.stl file and stores its triangles in a matrix
+% Usage: [ triangles, normals, name ] = ReadSTL(name)
+% Nicolas Broguiere, first version 3.1.2012, last edit 17.91.2022.
 
     ID=fopen(name);
+    %Import. %f means 'keep the float', and %*s means 'ignore the string'. 
+    % Different OS/soft use just \n or \r\n to mark the end of lines. 
+    % Since we have no use for lines anyway, just add these characters to delimiters to skip them altogether.
+    data=textscan(ID, '%*s %*s %f %f %f %*s %*s %*s %f %f %f %*s %f %f %f %*s %f %f %f %*s %*s','HeaderLines',1,'Delimiter',{' ','\t','\r','\n'},'MultipleDelimsAsOne',1);
 
-    textscan(ID, '%*[^\n]', 1);     % Get rid of the first line. 
-    data=textscan(ID, '%*s %*s %f %f %f \n %*s %*s \n %*s %f %f %f \n %*s %f %f %f \n %*s %f %f %f \n %*s \n %*s \n');      %Import. %f means 'keep the float', and %*s means 'ignore the string'.
-    data{1}=data{1}(1:(end-1));     % Get rid of the last lines, mistaken for values
-    data{2}=data{2}(1:(end-1));
-    data{3}=data{3}(1:(end-1));
+    %data{1}=data{1}(1:(end-1));     % Get rid of the last lines, mistaken for values
+    %data{2}=data{2}(1:(end-1));
+    %data{3}=data{3}(1:(end-1));
     data=cell2mat(data);            % Convert to a proper matrix of numbers from the cell array returned
 
     normals=data(:,1:3);
